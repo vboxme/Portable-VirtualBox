1ed39a65b5de4b72e4f99bde9f7ae8efb9ebbac9
Deac2 <159959206+Deac2@users.noreply.github.com>
2024-10-08 02:30:29 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 @echo off
+@title Build Aut2Exe
+cmd /c taskkill /im Portable-VirtualBox_x86.exe /f 2>nul
+cmd /c taskkill /im Portable-VirtualBox_x64.exe /f 2>nul
 
 rem Unseting user variables
-set "aut2exe=source\Aut2Exe\Aut2exe.exe"
-set "sevenzip=source\i_data\tools\7za.exe"
+set "aut2exe=source\Aut2Exe\Aut2exe_x64.exe"
 
 rem Setting up the different folders used for building. %~dp0 is the folder of the build script itself (may not be the same as the working directory).
 set "input_folder=%~dp0"
@@ -11,7 +13,7 @@ set "build_folder=%input_folder%build"
 
 rem Find path for aut2exe
 rem If the user supplied a aut2exe path use it
-IF DEFINED aut2exe (
+IF DEFINED "%aut2exe%" (
 	echo Using user defind path to aut2exe
 	goto done_aut2exe
 )
@@ -35,52 +37,27 @@ IF not exist "%aut2exe%" (
     EXIT /B
 )
 
-
-rem Find path for sevenzip
-rem If the user supplied a sevenzip path use it
-IF DEFINED sevenzip (
-	echo Using user defind path to sevenzip
-	goto done_sevenzip
-)
-
-rem Try to find the sevenzip path.
-set "PPATH=%ProgramFiles%\7-Zip\7z.exe"
-IF exist "%PPATH%" (
-    set "sevenzip=%PPATH%"
-	goto done_sevenzip
-) 
-
-set "PPATH=%ProgramFiles(x86)%\7-Zip\7z.exe"
-IF exist "%PPATH%" (
-    set "sevenzip=%PPATH%"
-	goto done_sevenzip
-) 
-
-:done_sevenzip
-IF not exist "%sevenzip%" (
-    echo Can't locate 7-Zip. Is it installed? Pleas set the sevenzip variable if it is installed in a nonstandard path.
-    EXIT /B
-)
-
 echo aut2exe path: %aut2exe%
-echo sevenzip path: %sevenzip%
 
 rem Create build and release folders if needed.
 
 rem Make a copy of the file for easy compression later.
 rem xcopy /d /c /e /i "%input_folder%source\i_data" "%build_folder%\data\"
-Mkdir "%build_folder%" >nul 2>&1
+Mkdir "%build_folder%" 2>nul
 
 rem Compile Portable-VirtualBox.
 "%aut2exe%" /in "%input_folder%source\Portable-VirtualBox.au3" /out "%build_folder%\Portable-VirtualBox_x86.exe" /icon "%input_folder%source\VirtualBox.ico" /x86
 "%aut2exe%" /in "%input_folder%source\Portable-VirtualBox.au3" /out "%build_folder%\Portable-VirtualBox_x64.exe" /icon "%input_folder%source\VirtualBox.ico" /x64
+
+SET "_replace=%build_folder%"
+SET "build_folder=%_replace:)=^)%"
 
 echo ###############################################################################
 for %%i in ("%build_folder%\Portable-VirtualBox_x64.exe") do (
 echo Build new release as %build_folder%\Portable-VirtualBox_x64.exe
 echo Size: %%~zi bytes
 )
-for %%i in ("%build_folder%\Portable-VirtualBox_x86.exe") do (
+for %%i in ("%build_folder%\Portable-VirtualBox_x86-x64.exe") do (
 echo Build new release as %build_folder%\Portable-VirtualBox_x86.exe
 echo Size: %%~zi bytes
 )
